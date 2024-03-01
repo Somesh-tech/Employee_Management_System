29be8c875bafb201cecc13ae9676df56558fb6cf
Somesh G <someshgaremane@gmail.com>
2024-03-01 13:28:37 +0530

    New Update

--- company.py
+++ company.py
@@ -1,41 +1,71 @@
 from empployee import *
+while True:
+        def menu():
+                print(""" Please select anyone of the following: 
+                1. Add Employee
+                2. Remove Employee
+                3. Show Employee Details
+                4. Add Department
+                5. Remove Department
+                6. Show All Department Details
+                7. Show all the employees
+                """)
+                user_input = input("Please provide the option: ")
+                return user_input
+        choice = menu()
 
-def menu():
-    user_input = input(
-        """ Please select anyone of the following: 
-            1. Add Employee
-            2. Remove Employee
-            3. Show Employee Details
-            4. Add Department
-            5. Remove Department
-            6. Show All Department Details
-            7. Show all the employees
-        """)
-    return user_input
-input = menu()
+        def add_employee():
+                data = input("Provide Employee Name, Employee ID, Employee Title, Department Name  (Note: provide comma seperated values) ")
+                name, emp_id, title, dept_name = data.split(",")
+                dept = Department(dept_name)
+                details = dept.add_emp(name=name, emp_id=emp_id, title=title, dept=dept_name)
+                print(f"Employee has been added : {details}")
 
-if int(input) == 1 :
-    name, emp_id, title = input(str("Provide Employee Name, Employee ID, Employee Title  (Note: provide comma seperated values) ").split(","))
-    add = Department.add_employee(name=name, emp_id=emp_id, title=title)
 
-if int(input) == 2 :
-    name, emp_id, title = input(str("Provide Employee Name"))
-    add = Department.remove_emp(emp_name=name)
+        def delete_employee():
+                data = input("Please provide employee ID : ")
+                details = Department.remove_emp(data)
+                print(details)
 
-if int(input) == 3 :
-    emp_id = input(str("Provide Employee ID "))
-    add = Employee.display_emp_detials(emp_id=emp_id)
+        def employee_list():
+                emp_id = input(str("Provide Employee ID "))
+                if emp_id in employee_details.keys():
+                        print(employee_details[emp_id])
+                else: print(f"No Employee with id :{emp_id}")        
 
-if int(input) == 4 :
-    dept_name, dept_id = input(str("Provide Department Name, Department ID (Note: provide comma seperated values) ").split(","))
-    add = add_dept(dept_name=dept_name, dept_id=dept_id)
+        def new_dept():
+                name = input("Please provide the Department Name: ")
+                try:
+                        add_dept(name)
+                        print("successfully added !")
+                except Exception as e:
+                        print(f"{e}")
 
-if int(input) == 5:
-    name = input(str("Provide Department Name" ))
-    add = remove_dept(dept_name = name)
+        def delete_dept():
+                name = input("Please provide the Department Name: ")
+                try:
+                        remove_dept(name)
+                        print("successfully Removed !")
+                except Exception as e:
+                        print(f"{e}")
 
-if int(input) == 6 :
-    print(display_dept())
+        def all_dept():
+                print(dept_details)
 
-if int(input) == 7:
-    print(Department.get_all_emp())
+        def all_emp():
+                print(employee_details)
+
+        if (choice) == '1' :
+                add_employee()
+        if (choice) == '2' :
+                delete_employee()
+        if (choice) == '3' :
+                employee_list()
+        if (choice) == '4' :
+                new_dept()
+        if (choice) == '5':
+                delete_dept()
+        if (choice) == '6' :
+                print(display_dept())
+        if (choice) == '7':
+                print(employee_details)
--- empployee.py
+++ empployee.py
@@ -1,54 +1,90 @@
+company = {}
+employee_details = {}
+dept_details = {}
 
-company={}
-employee_details={}
 
 class Employee:
-    def __init__(self, name, emp_id, title, dept):
-        self.name = name
+    def __init__(self, name:str, emp_id:int, title:str, dept:str):
+        self.emp_name = name
         self.emp_id = emp_id
-        self.title = title
-        self.dept = dept
-        employee_details[self.emp_id] = {"Name": self.name, "Title":self.title, "Department": self.dept}
+        self.emp_title = title
+        self.emp_dept = dept
+        if self.emp_id not in employee_details:
+            employee_details[self.emp_id] = {
+                "Name": self.emp_name,
+                "Title": self.emp_title,
+                "Department": self.emp_dept,
+            }
+        else:
+            print("Id already exist , please use another id")
 
     @classmethod
-    def display_emp_detials(self, emp_id):
-        print(employee_details[emp_id])    
+    def display_emp_detials(self, emp_id:int):
+        print(employee_details[emp_id])
 
     def __str__(self):
-        return {"Employee Name": {self.name}, 
-                "Employee ID": {self.emp_id}}
+        return {"Employee Name": {self.emp_name}, "Employee ID": {self.emp_id}}
+
 
 class Department(Employee):
-    def __init__(self, dept_name):
+    def __init__(self, dept_name:str):
         self.dept_name = dept_name
         self.employee_list = []
 
-    @classmethod    
-    def add_emp(self, name, emp_id, title):
-        new_employee = super().__init__(name, emp_id, title, self.dept_name)
-        self.employee_list.append(new_employee)
-        
     @classmethod
-    def remove_emp(self, emp_name):
-        for emp_name in self.employee_list:
+    def add_emp(self, name:str, emp_id:int, title:str, dept:str):
+        new_employee = Employee(name=name, emp_id=emp_id, title=title, dept=dept)
+        dept_details[new_employee.emp_dept] = [
+            new_employee.emp_title,
+            new_employee.emp_name,
+            new_employee.emp_id,
+        ]
+        if emp_id not in employee_details.keys():
+            employee_details[emp_id] = [
+                new_employee.emp_title,
+                new_employee.emp_name,
+                new_employee.emp_dept,
+            ]
+        else:
+            ("Duplicate emp_id cannot exists !")
+        print(employee_details)
+        return {
+            "Emp_name": new_employee.emp_name,
+            "Emp_id": new_employee.emp_id,
+            "Emp_title": new_employee.emp_title,
+        }
+
+    @staticmethod
+    def remove_emp(emp_id:int):
+        if emp_id in employee_details:
             try:
-                self.employee_list.remove(emp_name)   
-            except ValueError as e:
-                return (f"No employee was found for id:{emp_name}")
-            
+                employee_details.pop(emp_id)
+            except Exception as e:
+                return f"{e}"
+        else:
+            return f"No employee was found for id:{emp_id}"
+
     @classmethod
     def get_all_emp(self):
         return self.employee_list
-    
-def add_dept(department,dept_id):
-    company[f"{department}"] = Department(dept_name = department, dept_id = dept_id)
 
-def remove_dept(dept_name):
+
+def add_dept(department :str):
+    new_dept = Department(dept_name=department)
+    for dept, details in dept_details.items():
+        if dept == new_dept.dept_name:
+            company[f"{department}"] = details
+        else:
+            company[f"{department}"] = None
+            print("Its a new Department hence no employees are allocated as of now. ")
+
+
+def remove_dept(dept_name:str):
     if dept_name in company:
         company.pop(dept_name)
     else:
         print(f"Department {dept_name} not found in the company")
 
-def display_dept(company):
-    for dept_name,dept_details in company.items():
-        print(f"{dept_name} : {dept_details}")
+
+def display_dept():
+    print(f"{dept_details}")
